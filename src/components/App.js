933b124df70dc051437e85a9d0ee8aada046ee63
MohammadOwais10 <owais0945@gmail.com>
2022-01-28 20:09:19 +0530

    Setting up Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-
+import {Routes , Route } from 'react-router-dom';     /****In update "Switch" change into "Routes" */
+import Navbar from './Navbar';
+import Home from './Home';
+import CreatePost from './CreatePost';
+import PostDetail from './PostDetail';
+// import {Navbar,Home,CreatePost,PostDetail} from './';   /******This format not support in Update  */
+  
 function App() {
   return (
-    <div className="App">
-     React Blog
+    <div className="container">
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />  {/*in Update "component" change into "elememt"  , we cant write name in simple curly bracket but also use  "<>" this like that "{<Home>}*/}
+        <Route exact path='/post/:postId' element={<PostDetail/>} />
+        <Route exact path='/create-post' element={<CreatePost/>} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
