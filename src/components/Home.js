feeb4adcf0ea37dc2e7685fe63f1c4becbb9940d
MohammadOwais10 <owais0945@gmail.com>
2022-01-29 12:38:47 +0530

    Implementing Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { firestore } from '../firebase';
+
 function Home() {
-  return <div className="">Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        console.log('posts',posts);
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>React Blog</h1>
+      <div id="blog-by">Owais</div>
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/pst/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
